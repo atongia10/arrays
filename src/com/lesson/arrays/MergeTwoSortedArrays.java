b6c96785fd75e53c3c1c0a3da54444b04c87c2d8
atongia10 <atongia10@gmail.com>
2020-01-05 21:55:37 -0500

    Merge two sorted arrays.

---
+++ src/com/lesson/arrays/MergeTwoSortedArrays.java
@@ -0,0 +1,43 @@
+package com.lesson.arrays;
+
+import java.util.Arrays;
+
+public class MergeTwoSortedArrays {
+
+    public static void main(String[] args) {
+
+        int[] arr1 = {1, 3, 4, 15};
+        int[] arr2 = {0, 2, 6, 7, 8, 12};
+
+        Arrays.stream(mergeArrays(arr1, arr2))
+            .forEach(System.out::println);
+
+    }
+
+    private static int[] mergeArrays(int[] arr1, int[] arr2) {
+        int [] result = new int[arr1.length + arr2.length];
+
+        int i =0;
+        int j=0;
+        int k=0;
+
+        while (i < arr1.length && j < arr2.length) {
+         if(arr1[i] <= arr2[j]){
+             result[k++]=arr1[i++];
+         } else {
+             result[k++]=arr2[j++];
+         }
+        }
+
+        while (i<arr1.length){
+            result[k++]=arr1[i++];
+        }
+
+        while (j<arr2.length){
+            result[k++]=arr2[j++];
+        }
+
+        return result;
+    }
+
+}
