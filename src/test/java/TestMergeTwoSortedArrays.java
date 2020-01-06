66ed0bbbfd4a18c451d027e02ea414ae38909ae8
atongia10 <atongia10@gmail.com>
2020-01-05 22:11:10 -0500

    Adding Test Cases

--- src/test/java/TestMergeTwoSortedArrays.java
+++ src/test/java/TestMergeTwoSortedArrays.java
@@ -25,4 +25,13 @@ public class TestMergeTwoSortedArrays {
         assertArrayEquals(result, tester.mergeArrays(arr1,arr2));
     }
 
+    @Test
+    public void compareMergeTwoSortedArraysMethod1() {
+        int [] arr1 = {1, 3, 4, 5, 5};
+        int [] arr2 = {2, 6, 7, 8};
+        int [] result = {1,2,3,4,5,5,6,7,8};
+        MergeTwoSortedArrays tester = new MergeTwoSortedArrays();
+        assertArrayEquals(result, tester.mergeArrays(arr1,arr2));
+        System.out.println(result);
+    }
 }
